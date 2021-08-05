c93116653ee98ab1765b0e76ce4f42f1ac09ebe9
Raafi Rahman <rafhay101@gmail.com>
2021-08-04 22:26:22 -0400

    Update db.js

--- Server/db.js
+++ Server/db.js
@@ -2,10 +2,10 @@ const Pool = require('pg').Pool;
 
 const pool = new Pool({
     user: "postgres",
-    password: "AWSPassword",
+    password: "PostgressPassword",
     host: "http://database-1.cnv6v0f9xbl3.us-east-2.rds.amazonaws.com/",
     port: 5432,
-    database: "postgres"
+    database: "AWS"
 })
 
 module.exports = pool;
