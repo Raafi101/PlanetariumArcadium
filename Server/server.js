29179929116f63d2749f2bf8238f6ea1bfabee5c
Raafi Rahman <rafhay101@gmail.com>
2021-08-07 17:51:58 -0400

    Updates

--- Server/server.js
+++ Server/server.js
@@ -1,28 +1,43 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const pool = require('./db');
+const Pool = require('pg').Pool;
 
 //Middleware
 app.use(cors());
 
-//Routes
-app.get('/bodies', async (req, res) => {
-    try {
-        const {name} = req.query;
-
-        //HostName PlanetName
-
-
-        const bodies = await pool.query("select * from planets where host_name || ' ' || planet_letter ILIKE $1", [`%${name}%`]);
-
-        res.json(bodies.rows);
-    } catch (error) {
-        console.log(error.message);
-    }
+//Connection
+const pool = new Pool({
+    user: "postgres",
+    password: "PostgressPassword",
+    host: "database-1.cnv6v0f9xbl3.us-east-2.rds.amazonaws.com",
+    port: 5432,
+    database: "postgres"
 })
 
-const port = process.env.PORT || 5000;
+//Routes
+app.get('/', async (req, res) => {
+    res.send("/. Now do the thing")
+})
+
+app.get('/bodies', async (req, res) => {
+    res.send("/bodies")
+    
+    //Get Query
+    const {name} = req.query;
+
+    //Query
+    const bodies = await pool.query("select * from planets where host_name || ' ' || planet_letter ILIKE $1", [`%${name}%`]);
+
+    //Set Header
+    res.setHeader("content-type", "application/json")
+
+    //Response
+    res.send(JSON.stringify(bodies.rows));
+    console.log(bodies.rows);
+})
+
+const port = process.env.PORT || 5000
 app.listen(port, () => {
     console.log(`Server is starting on port ${port}`)
 })
